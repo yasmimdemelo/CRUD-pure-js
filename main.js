ae388fb09fdc4d61e205d7839d40c6bb14b0c1fd
Yasmim <yasmimdmf95@gmail.com>
2022-12-19 14:25:32 -0300

    Added a submit EventListener for the form

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
+
+form.addEventListener("submit", (e) => {   /* (e) representa o elemento que foi adicionado para o event listener*/
+    e.preventDefault();
+    console.log("button clicked");
+    
+    formValidation();
+});
+
+let formValidation = () => {};
