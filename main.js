e4f23c9c2aaefd5ce62b2b6ca9240eb5a8bf6ba4
Yasmim <yasmimdmf95@gmail.com>
2022-12-19 15:15:11 -0300

    Organized names and ids in files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
-let tasks = document.getElementById("tasks");
+let posts = document.getElementById("posts");
 
 form.addEventListener("submit", (e) => {   /* (e) representa o elemento que foi adicionado para o event listener*/
     e.preventDefault();                     /* preventDefault()= Cancela o evento, significando que a ação padrão que pertence ao evento não ocorrerá*/
@@ -11,7 +11,7 @@ form.addEventListener("submit", (e) => {   /* (e) representa o elemento que foi 
 });
 
 let formValidation = () => {
-    if (input.value === " ") {
+    if (input.value === "") {
         msg.innerHTML = "Task cannot be blank"; 
         console.log("failure");
     } else {
