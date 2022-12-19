3a3ba57107c652d889b00af3230e2c183f2b66b9
Yasmim <yasmimdmf95@gmail.com>
2022-12-19 14:54:06 -0300

    Add functio for declaration if else in formValidation

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 
 form.addEventListener("submit", (e) => {   /* (e) representa o elemento que foi adicionado para o event listener*/
-    e.preventDefault();
+    e.preventDefault();                     /* preventDefault()= Cancela o evento, significando que a ação padrão que pertence ao evento não ocorrerá*/
     console.log("button clicked");
     
     formValidation();
 });
 
-let formValidation = () => {};
+let formValidation = () => {
+    if (input.value === " ") {
+        msg.innerHTML = "Task cannot be blank"; 
+        console.log("failure");
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+    }
+};
