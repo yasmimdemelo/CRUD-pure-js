092593c2b10938c56611b38a5e69902cb24bf673
Yasmim <yasmimdmf95@gmail.com>
2022-12-21 18:22:18 -0300

    Organized the commentaries

--- main.js
+++ main.js
@@ -3,30 +3,30 @@ let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
 
-form.addEventListener("submit", (e) => {   /* (e) representa o elemento que foi adicionado para o event listener*/
-    e.preventDefault();                     /* preventDefault()= Cancela o evento, significando que a ação padrão que pertence ao evento não ocorrerá*/
+form.addEventListener("submit", (e) => { //(e) representa o elemento que foi adicionado para o event listener.
+    e.preventDefault(); //preventDefault()= Cancela o evento, significando que a ação padrão que pertence ao evento não ocorrerá.
     console.log("button clicked");
     
     formValidation();
 });
 
 let formValidation = () => {
-    if (input.value === "") {     /*O operador de igualdade estrita (===) verifica se seus dois operandos são iguais, retornando um resultado booleano */
+    if (input.value === "") { //O operador de igualdade estrita (===) verifica se seus dois operandos são iguais, retornando um resultado booleano.
         msg.innerHTML = "Task cannot be blank"; 
         console.log("failure");
     } else {
         console.log("success");
         msg.innerHTML = "";
-        acceptData();  /* Add aqui depois que criar o object data */
+        acceptData(); //Add aqui depois que criar o object data.
     }
 };
 
-let data = {};  /* Quaisquer dados que recebemos em um campo de entrada vamos armazenar em um objeto */
+let data = {}; //Quaisquer dados que recebemos em um campo de entrada vamos armazenar em um objeto.
 let acceptData = () => {
     data["text"] = input.value;
     console.log(data);
 
-    createPost();       /* Add esta function após ter criado a variavel createPost */
+    createPost(); //Add esta function após ter criado a variavel createPost.
 };
 
 /*Literais de modelos usam acento graves (``) para definir uma string
@@ -51,6 +51,6 @@ let deletePost = (e) => {
 
 //function edit
 let editPost = (e) => {
-    input.value = e.parentElement.previousElementSibling.innerHTML;  /* previousElementSibling =  propriedade somente leitura retorna o Element imediatamente anterior ao especificado na lista de filhos de seu pai  */
+    input.value = e.parentElement.previousElementSibling.innerHTML; //previousElementSibling =  propriedade somente leitura retorna o Element imediatamente anterior ao especificado na lista de filhos de seu pai
     e.parentElement.parentElement.remove();
 };
